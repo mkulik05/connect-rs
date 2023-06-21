392ac7d2c800bebefb74568774b714f12fb39ec2
mkulik05 <mishakulik2@gmail.com>
2023-06-21 20:02:46 +0300

    swithced to post request from websockets

--- server/src/worker.js
+++ server/src/worker.js
@@ -5,38 +5,23 @@ let get_free_wg_id = async (redis, room_id) => {
     return "10.8." + Math.floor(len / 255) + "." + len % 256
 }
 
-async function handleRequest(request, redis) {
-    const upgradeHeader = request.headers.get('Upgrade');
 
-    if (!upgradeHeader || upgradeHeader !== 'websocket') {
-        console.log("stupid user")
-        return new Response('Expected Upgrade: websocket', { status: 426 });
-    }
-    const wsPair = new WebSocketPair();
-    const [client, server] = Object.values(wsPair);
-
-    server.accept();
-
-    server.addEventListener('message', async (event) => {
-
-        console.log(`[message]: ${event.data}`);
-        let data = JSON.parse(event.data);
-        let room_name = data.room_name;
-        let wg_ip = await get_free_wg_id(redis, room_name)
-        data.peer_info.wg_ip = wg_ip;
-        await redis.lpush(room_name, JSON.stringify(data.peer_info));
-        await redis.publish(room_name, JSON.stringify({"JoinReqMsg": data})) 
-        server.send(JSON.stringify({"WgIpMsg" : wg_ip}))
-    });
-    return new Response(null, {
-        status: 101,
-        webSocket: client,
-    });
-}
 
 export default {
     async fetch(request, env) {
         let redis = Redis.fromEnv(env)
-        return await handleRequest(request, redis)
+        if (request.method === "POST") {
+            let data = JSON.parse(await request.text());
+            let room_name = data.room_name;
+            let wg_ip = await get_free_wg_id(redis, room_name)
+            data.peer_info.wg_ip = wg_ip;
+            await redis.lpush(room_name, JSON.stringify(data.peer_info));
+            await redis.publish(room_name, JSON.stringify({"JoinReqMsg": data})) 
+            return new Response(JSON.stringify({"WgIpMsg" : wg_ip}));
+          } else {
+            return new Response('Expected post request', {
+                status: 403,
+            });
+          }
     },
 };
