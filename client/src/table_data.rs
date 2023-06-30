2c8f4f6172dda9230994301d578fea5932fc99ec
mkulik05 <mishakulik2@gmail.com>
2023-06-30 19:51:07 +0300

    added log messages printed at top of table

--- client/src/table_data.rs
+++ client/src/table_data.rs
@@ -1,6 +1,9 @@
 use crate::app_backend::{DisconnectReq, PeerInfo};
-use crate::update_table;
-use std::io::stdout;
+use crate::toml_conf::Config;
+use crate::TableMessage;
+use crossterm::style::Print;
+use crossterm::QueueableCommand;
+use std::io::{stdout, Write};
 use std::sync::{
     atomic::{AtomicU16, Ordering},
     Arc,
@@ -20,13 +23,17 @@ pub struct TableItem {
 pub struct TableData {
     pub data: Arc<RwLock<Vec<TableItem>>>,
     pub calc_ping_task: Option<JoinHandle<()>>,
+    pub my_wg_ip: RwLock<String>,
+    pub sender: tokio::sync::mpsc::Sender<TableMessage>,
 }
 
 impl TableData {
-    pub fn new() -> Self {
+    pub fn new(sender: tokio::sync::mpsc::Sender<TableMessage>) -> Self {
         TableData {
             data: Arc::new(RwLock::new(Vec::new())),
             calc_ping_task: None,
+            my_wg_ip: RwLock::new("".to_string()),
+            sender,
         }
     }
     pub async fn add_peer(&self, info: &PeerInfo) {
@@ -39,8 +46,8 @@ impl TableData {
         {
             self.data.write().await.push(displ_data);
         }
-        if let Err(e) = update_table(&mut stdout(), self).await {
-            eprintln!("Error during table update: {:?}", e);
+        if let Err(e) = self.sender.send(TableMessage::UpdateTable).await {
+            eprintln!("Error sending update request: {:?}", e);
         };
     }
     pub async fn remove_peer(&self, info: &DisconnectReq) {
@@ -50,10 +57,51 @@ impl TableData {
                 .await
                 .retain(|peer| *peer.pub_key != info.pub_key);
         }
-        if let Err(e) = update_table(&mut stdout(), self).await {
-            eprintln!("Error during table update: {:?}", e);
+        if let Err(e) = self.sender.send(TableMessage::UpdateTable).await {
+            eprintln!("Error sending update request: {:?}", e);
         };
     }
+
+    pub async fn update_table(&self) -> Result<u8, anyhow::Error> {
+        let mut lines_n = 5;
+        let mut stdout = stdout();
+        let peers = &(*self.data.read().await);
+        // eprintln!("3");
+        //tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+        println!(
+            "\nRoom: {}\n\n{:<15} {:^15} {:^8}",
+            Config::global().room.room_name.as_str(),
+            "USERNAME",
+            "Local IP",
+            "PING"
+        );
+        println!(
+            "{:<15} {:^15} {:^8}",
+            Config::global().room.username.as_str(),
+            self.my_wg_ip.read().await,
+            "0 ms"
+        );
+        for peer in peers {
+            lines_n += 1;
+            let ping_millis = peer.ping.load(Ordering::SeqCst);
+            let ping_str = match ping_millis {
+                u16::MAX => "-".to_string(),
+                1000u16.. => ">999 ms".to_string(),
+                _ => format!("{:?} ms", ping_millis),
+            };
+            let mut username = peer.username.clone();
+            username.truncate(15);
+            stdout.queue(Print(format!(
+                "{:<15} {:^15} {:^8}\n",
+                &username, peer.wg_ip, ping_str
+            )))?;
+
+        }
+
+        stdout.flush()?;
+        Ok(lines_n)
+    }
+
     pub fn calc_ping(&mut self) {
         let data = self.data.clone();
         self.calc_ping_task.get_or_insert(tokio::spawn(async move {
